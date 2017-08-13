5ffc86521f65816f69769ff181f37484d15c2192
Allan James Lapid <ajLapid718@gmail.com>
2017-08-13 00:30:28 -0400

    Add a Ruby Comment Regarding Dependent Destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :posts
-  has_many :comments
+  has_many :posts #, dependent: :destroy || comment this in if we want to delete a user's posts if the user cancels their account (but some websites just keep them around and replace the username with "deleted")
+  has_many :comments #, dependent: :destroy || nearly identical logic as the comment in line 7
 end
