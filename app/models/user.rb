8c484a681bc095d7eeff3f314a6f126b72b0f9e2
Allan James Lapid <ajLapid718@gmail.com>
2017-08-15 04:45:22 -0400

    Add dependent destroy to users for account cancellation purposes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :name
 
-  has_many :posts #, dependent: :destroy || comment this in if we want to delete a user's posts if the user cancels their account (but some websites just keep them around and replace the username with "deleted")
-  has_many :comments #, dependent: :destroy || nearly identical logic as the comment in line 7
+  has_many :posts, dependent: :destroy # || comment this in if we want to delete a user's posts if the user cancels their account (but some websites just keep them around and replace the username with "deleted")
+  has_many :comments, dependent: :destroy # || nearly identical logic as the comment in line 7
 end
