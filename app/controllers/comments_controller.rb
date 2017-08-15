033313e9f48530f4f95e101c6331038f1aa27de4
Allan James Lapid <ajLapid718@gmail.com>
2017-08-14 21:13:26 -0400

    Change caption on error message in view and improve controller-level logic and params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     @comment.user_id = current_user.id
+    @comment.post_id = @post.id
     if @comment.save
       redirect_to @post
     else
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :body, :user_id)
+      params.require(:post).permit(:title, :body)
     end
 end
