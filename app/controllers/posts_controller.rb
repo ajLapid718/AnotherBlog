72595d52b2f8adf981fbb9c08fe79fe36cc75506
Allan James Lapid <ajLapid718@gmail.com>
2017-08-17 03:01:55 -0400

    Add hover effect to page nav, change posts per page to 3, and change font size in page nav, and bring the page nav bar below posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   def index
     # @posts = Post.all.order('created_at DESC')
-    @posts = Post.order('created_at DESC').page(params[:page]).per(5)
+    @posts = Post.order('created_at DESC').page(params[:page]).per(3)
   end
 
   def new
